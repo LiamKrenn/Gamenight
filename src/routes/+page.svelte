4b676380ccf0aeab82d9d087062c2172000e814b
LiamKrenn <k.liam1224@gmail.com>
2024-07-25 12:18:55 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import LogoNoBackground from "$lib/icons/logo-no-background.svelte";
+
+
+</script>
+
+<div class="w-full h-full flex items-center justify-center">
+  <LogoNoBackground class="opacity-20 w-96"/>
+</div>
