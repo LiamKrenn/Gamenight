690a85085224ffefc9a6fc5911944252761923d5
LiamKrenn <k.liam1224@gmail.com>
2024-07-31 12:37:19 +0200

    log response

--- src/routes/(auth)/oauth/google/callback/+server.ts
+++ src/routes/(auth)/oauth/google/callback/+server.ts
@@ -8,7 +8,7 @@ export async function GET(req) {
       'cookie': `session=${req.cookies.get('session')}`
     }
   })
-	console.log(jj);
+	console.log(await jj.json());
   
   let session = '1234567890';
 	let resp = new Response(null, {
