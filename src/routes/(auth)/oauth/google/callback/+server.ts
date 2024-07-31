c3ca5d9709a01c72bf10d01c5aaf2f18b930889e
LiamKrenn <k.liam1224@gmail.com>
2024-07-31 11:44:54 +0200

    auth test

--- src/routes/(auth)/oauth/google/callback/+server.ts
+++ src/routes/(auth)/oauth/google/callback/+server.ts
@@ -1,10 +1,14 @@
+import { AUTH_URL } from '$env/static/private';
+
 export async function GET(req) {
-	console.log(req);
+  const params = new URLSearchParams(req.params)
+  let jj = await fetch(AUTH_URL + `oauth/google/callback?${params.toString()}`, )
+	console.log(jj);
   
   let token = '1234567890';
 	let resp = new Response(null, {
 		status: 302,
-		headers: { Location: '/', 'Set-Cookie': `token=${token}`, 'access-control-expose-headers': 'Set-Cookie' }
+		headers: { Location: '/', 'Set-Cookie': `token=${token}` }
 	});
 
 	return resp;
