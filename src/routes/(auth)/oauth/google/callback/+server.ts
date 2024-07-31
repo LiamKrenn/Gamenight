563c38249ceef88c85ceb4a9f7b275242a1df4f5
LiamKrenn <k.liam1224@gmail.com>
2024-07-31 12:33:48 +0200

    server side test

--- src/routes/(auth)/oauth/google/callback/+server.ts
+++ src/routes/(auth)/oauth/google/callback/+server.ts
@@ -1,7 +1,13 @@
 import { AUTH_URL } from '$env/static/private';
 
 export async function GET(req) {  
-  let jj = await fetch(AUTH_URL + `/oauth/google/callback${req.url.search}`, )
+  let jj = await fetch(AUTH_URL + `/oauth/google/callback${req.url.search}`, {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      'cookie': `session=${req.cookies.get('session')}`
+    }
+  })
 	console.log(jj);
   
   let session = '1234567890';
