fb3790eafa9c38835a28d4693cd2e5c3f05305fb
LiamKrenn <k.liam1224@gmail.com>
2024-07-31 11:25:23 +0200

    oauth set cookie experimental

--- src/routes/(auth)/oauth/google/callback/+server.ts
+++ src/routes/(auth)/oauth/google/callback/+server.ts
@@ -4,7 +4,7 @@ export async function GET(req) {
   let token = '1234567890';
 	let resp = new Response(null, {
 		status: 302,
-		headers: { Location: '/', 'Set-Cookie': `token=${token}` }
+		headers: { Location: '/', 'Set-Cookie': `token=${token}`, 'access-control-expose-headers': 'Set-Cookie' }
 	});
 
 	return resp;
