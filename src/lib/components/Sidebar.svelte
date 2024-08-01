717960e144f458c0f4fa0673e49bd08f9c67685c
LiamKrenn <k.liam1224@gmail.com>
2024-08-01 20:21:48 +0200

    better scrollbar for sidebar

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
+	import 'overlayscrollbars/overlayscrollbars.css';
+	import { OverlayScrollbars } from 'overlayscrollbars';
+	import { onMount } from 'svelte';
 	let className: string = '';
 	export { className as class };
 	export let open = false;
+
+	let mainArea: HTMLDivElement;
+	onMount(() => {
+		const osInstance = OverlayScrollbars(mainArea, {});
+	});
 </script>
 
 <div
@@ -9,7 +17,10 @@
 		? 'left-0'
 		: '-left-[550px]'}"
 >
-	<div class="absolute h-full w-full border-slate-700 bg-slate-800 duration-150 xs:border-r-2 overflow-auto">
+	<div
+		bind:this={mainArea}
+		class="cscroll absolute h-full w-full overflow-y-auto overflow-x-hidden border-slate-700 bg-slate-800 duration-150 xs:border-r-2"
+	>
 		<slot />
 	</div>
 </div>
