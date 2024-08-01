1493c203efdc90091506f3f53aab334acba38520
LiamKrenn <k.liam1224@gmail.com>
2024-08-01 17:16:15 +0200

    overflow fix

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -9,7 +9,7 @@
 		? 'left-0'
 		: '-left-[550px]'}"
 >
-	<div class="absolute h-full w-full border-slate-700 bg-slate-800 duration-150 xs:border-r-2">
+	<div class="absolute h-full w-full border-slate-700 bg-slate-800 duration-150 xs:border-r-2 overflow-scroll">
 		<slot />
 	</div>
 </div>
