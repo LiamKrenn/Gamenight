445cd0425b31fd0b1dd6e07ec1a1c86b5c07cfb0
LiamKrenn <k.liam1224@gmail.com>
2024-08-01 17:09:34 +0200

    sidebar improvements

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -5,9 +5,9 @@
 </script>
 
 <div
-	class="{className} fixed !z-30 h-full w-full ease-in-out xs:max-w-96 {open
+	class="{className} absolute !z-30 h-full w-full ease-in-out xs:max-w-96 {open
 		? 'left-0'
-		: 'invisible -left-full xs:-left-96'}"
+		: '-left-[550px]'}"
 >
 	<div class="absolute h-full w-full border-slate-700 bg-slate-800 duration-150 xs:border-r-2">
 		<slot />
