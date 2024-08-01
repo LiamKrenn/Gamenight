be534c2b1ae12c3fa44b901588b413e5e667e240
LiamKrenn <k.liam1224@gmail.com>
2024-08-01 16:48:00 +0200

    invisible sidebar frame blocking ui fix

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -4,11 +4,12 @@
 	export let open = false;
 </script>
 
-<div class="{className} absolute !z-30 h-full w-full xs:max-w-96">
-	<div
-		class="absolute h-full w-full border-slate-700 bg-slate-800 duration-150 ease-in-out xs:border-r-2
-    {open ? 'left-0' : 'invisible -left-full'}"
-	>
+<div
+	class="{className} fixed !z-30 h-full w-full ease-in-out xs:max-w-96 {open
+		? 'left-0'
+		: 'invisible -left-full xs:-left-96'}"
+>
+	<div class="absolute h-full w-full border-slate-700 bg-slate-800 duration-150 xs:border-r-2">
 		<slot />
 	</div>
 </div>
