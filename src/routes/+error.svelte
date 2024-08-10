9dd47efb166f11e77613467b4296c19c2bbe9629
LiamKrenn <k.liam1224@gmail.com>
2024-08-10 20:16:55 +0200

    bugfix only working on base level

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,6 +4,6 @@
 
 <div class="w-full h-full flex flex-col items-center justify-center">
   <h1 class="text-4xl font-semibold mb-8 text-center">{$page.status}: {$page.error?.message}</h1>
-  <img class="w-[52rem] max-w-[80%]" src="error.gif" alt="Error GIF"/>
+  <img class="w-[52rem] max-w-[80%]" src="/error.gif" alt="Error GIF"/>
   <p class="text-2xl mt-8 text-center">Go back to the <a href="/" class="underline">homepage</a></p>
 </div>
