dee12dfcb6bf63737a9aca788e22d9a0e71e404a
LiamKrenn <k.liam1224@gmail.com>
2024-08-10 18:40:37 +0200

    added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+</script>
+
+<div class="w-full h-full flex flex-col items-center justify-center">
+  <h1 class="text-4xl font-semibold mb-8 text-center">{$page.status}: {$page.error?.message}</h1>
+  <img class="w-[52rem] max-w-[80%]" src="error.gif" alt="Error GIF"/>
+  <p class="text-2xl mt-8 text-center">Go back to the <a href="/" class="underline">homepage</a></p>
+</div>
