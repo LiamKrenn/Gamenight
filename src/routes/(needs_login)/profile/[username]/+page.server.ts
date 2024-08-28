bd537244c0bdd2df6f1b6d023ea1269655942991
JohnGrubba <nicjontrickshots@gmail.com>
2024-08-28 18:33:42 +0200

    Profile Page Ignore Bun Lockfile

--- src/routes/(needs_login)/profile/[username]/+page.server.ts
+++ src/routes/(needs_login)/profile/[username]/+page.server.ts
@@ -1,13 +1,21 @@
 import { PRIVATE_AUTH_URL } from '$env/static/private';
+import { PUBLIC_AUTH_URL } from '$env/static/public';
 import type { PageServerLoad } from './$types';
 
 export const load = (async (event) => {
-    let userResp = await fetch(PRIVATE_AUTH_URL + '/profile/profile/' + event.params.username);  
-    console.log(userResp);
-    let user = await userResp.json();
-    console.log(user);
-    
-    return {
-      user: user
-    };
+  let userResp = await fetch(PRIVATE_AUTH_URL + '/profile/' + event.params.username);
+  console.log(userResp);
+  let user = await userResp.json();
+  console.log(user);
+
+  // Check if Profile Picture Available
+  const pic_available = (await fetch(`${PUBLIC_AUTH_URL}/cdn/${user._id}.webp`, {
+    method: 'GET'
+  })).status === 200
+  console.log(pic_available);
+
+  return {
+    user: user,
+    pic_available: pic_available
+  };
 }) satisfies PageServerLoad;
--- src/routes/(needs_login)/profile/[username]/+page.svelte
+++ src/routes/(needs_login)/profile/[username]/+page.svelte
@@ -1,9 +1,41 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
+	import { PUBLIC_AUTH_URL } from '$env/static/public';
+	import type { PageData } from './$types';
 
-    console.log(data);
+	export let data: PageData;
+
+	console.log(data);
 </script>
 
-{data.user.username}
+<div class="p-7">
+	<div
+		class="flex flex-col justify-center
+gap-5 text-white lg:flex-row"
+	>
+		<div
+			class="flex max-w-[700px]
+            flex-1 flex-row
+            gap-7 rounded-xl bg-gray-700 p-6 shadow-2xl
+            "
+		>
+			{#if data.pic_available}
+				<img src="{PUBLIC_AUTH_URL}/cdn/{data.user._id}.webp" alt="Profile" />
+			{:else}
+				<img src="/favicon.png" alt="Profile" />
+			{/if}
+			<div>
+				<p class="text-4xl font-bold">{data.user.username}</p>
+				<p class="mb-2">
+					with us since {new Date(data.user.createdAt).toLocaleString('de-DE', {
+						year: 'numeric',
+						month: 'short',
+						day: '2-digit'
+					})}
+				</p>
+				<p class="text-lg">🥇 <span class="text-green-400">1 (100%)</span></p>
+				<p class="text-lg">🎮 <span class="text-blue-400">1</span></p>
+				<p class="text-lg">❌ <span class="text-red-400">0 (0%)</span></p>
+			</div>
+		</div>
+	</div>
+</div>
