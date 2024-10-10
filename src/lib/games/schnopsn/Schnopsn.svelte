6f4b7e3a3ef5874ebfc4821ebf918b86b6eddb8a
LiamKrenn <k.liam1224@gmail.com>
2024-10-10 18:04:02 +0200

    schnopsn demo

--- src/lib/games/schnopsn/Schnopsn.svelte
+++ src/lib/games/schnopsn/Schnopsn.svelte
@@ -24,12 +24,20 @@
 	import SchnapsenClient, { SchnapsenClientBuilder } from 'gn-schnapsen-client';
 	import { wait } from './SchnopsnAnimation';
 	import { browser } from '$app/environment';
-	import { cardToCardType, ownHand } from './Schnopsn';
+	import {
+		cardToCardType,
+		opponentHand,
+		opponentPlayedCard,
+		ownHand,
+		ownPlayedCard
+	} from './Schnopsn';
 
 	if (browser) {
+		const own_id = 'saus' + Math.random();
+
 		let instance = new MatchMaker(
 			'https://matchmaking.jjhost.at',
-			'saus' + Math.random(),
+			own_id,
 			new SchnapsenClientBuilder()
 		);
 		let info: SearchInfo = {
@@ -51,15 +59,10 @@
 
 			let onActive = async () => {
 				console.log('Playing Card');
-
-				await wait(1000);
-
+				let coolcards = cardToCardType(client.cardsAvailable);
+				console.log('Available: ' + coolcards);
+				$ownHand = coolcards;
 				setTimeout(() => {
-					let coolcards = cardToCardType(client.cardsAvailable);
-					console.log('Available: ' + coolcards);
-					$ownHand = coolcards;
-					console.log('ADAAFDSF', browser);
-
 					console.log('Trump: ' + JSON.stringify(client.trump));
 					console.log('Playable: ' + JSON.stringify(client.cardsPlayable));
 					client.playCard(
@@ -70,7 +73,6 @@
 
 			client.on('self:allow_draw_card', async () => {
 				console.log('Drawing card');
-				await wait(1000);
 				setTimeout(() => {
 					client.drawCard();
 				}, 1000);
@@ -79,9 +81,23 @@
 			client.on('self:allow_play_card', onActive);
 
 			client.on('play_card', (event) => {
+				if (event.data.user_id === own_id) {
+					$ownHand = $ownHand.filter((card) => card !== cardToCardType([event.data.card])[0]);
+					$ownPlayedCard = cardToCardType([event.data.card])[0];
+				} else {
+					$opponentPlayedCard = cardToCardType([event.data.card])[0];
+				}
+
 				console.log(
 					`Player ${event.data.user_id} played ${event.data.card.suit} ${event.data.card.value}`
 				);
+			});
+
+			client.on('trick', (event) => {
+				setTimeout(() => {
+					$ownPlayedCard = null;
+					$opponentPlayedCard = null;
+				}, 1000);
 			});
 
 			client.on('self:trick', (trick) => {
@@ -90,9 +106,15 @@
 
 			client.on('self:card_available', (event) => {
 				// console.log(event);
+				let coolcards = cardToCardType(client.cardsAvailable);
+				console.log('Available: ' + coolcards);
+				$ownHand = coolcards;
 			});
 
 			client.on('self:card_unavailable', (event) => {
+				let coolcards = cardToCardType(client.cardsAvailable);
+				console.log('Available: ' + coolcards);
+				$ownHand = coolcards;
 				// console.log(event);
 			});
 
